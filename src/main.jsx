952607d2cfa4285f12f6132f191e2d6ea379cee4
DTdigital05 <dtdigital05@gmail.com>
2026-01-12 10:10:10 +0700

    Add missing postcss package

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <--- BẮT BUỘC PHẢI CÓ DÒNG NÀY
+import './index.css' // <-- DÒNG NÀY QUYẾT ĐỊNH MÀU SẮC CỦA WEB
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
